988593c350b010ef0be03ea904a0e2fca2c0eb02
M3morui <vivi.santa0320@gmail.com>
2025-02-12 19:16:57 +0100

    Update megoldasok.sql

--- adatbázis/megoldasok.sql
+++ adatbázis/megoldasok.sql
@@ -18,7 +18,11 @@ SELECT varos AS varos, lnv-lkv AS ingadozas
 FROM vizmerce;
  
 -- 16. feladat:
-
+SELECT nev, COUNT(vizmerce.id) AS merceszam FROM igazgatosag
+INNER JOIN vizmerce ON igazgatosag.id = vizmerce.igId
+GROUP BY nev;
   
 -- 17. feladat:
-
+SELECT AVG(vizAllas) AS atlag FROM vizmerce 
+INNER JOIN meres ON vizmerce.id = meres.vmId
+WHERE MONTH(nap) = 4 AND varos = 'Szolnok';
