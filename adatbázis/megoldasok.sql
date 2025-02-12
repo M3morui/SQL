971b85edc12a660a86e2e5040bcc91de0618f03b
M3morui <vivi.santa0320@gmail.com>
2025-02-12 15:36:52 +0100

    Add files via upload

---
+++ adatbázis/megoldasok.sql
@@ -0,0 +1,24 @@
+﻿-- A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!
+
+-- 10. feladat:
+
+CREATE DATABASE tisza;
+-- 12. feladat:
+
+DELETE FROM meres WHERE nap = "2020-03-27";
+-- 13. feladat:
+UPDATE vizmerce SET igId = '2' WHERE vizmerce.id = 3;
+
+-- 14. feladat:
+SELECT varos, MIN(nullPont) FROM vizmerce
+WHERE nullPont;
+
+-- 15. feladat:
+SELECT varos AS varos, lnv-lkv AS ingadozas 
+FROM vizmerce;
+ 
+-- 16. feladat:
+
+  
+-- 17. feladat:
+
